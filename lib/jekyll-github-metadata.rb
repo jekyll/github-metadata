d3170d341fe76efc48aabaa8c8a27a0da356e51f
Parker Moore <parkrmoore@gmail.com>
2014-09-17 22:09:36 -0700

    Pages env stuff

--- lib/jekyll-github-metadata.rb
+++ lib/jekyll-github-metadata.rb
@@ -75,7 +75,31 @@ module Jekyll
       end
     end
 
+    class Pages
+      class << self
+        def env
+          ENV.fetch('PAGES_ENV', 'dotcom')
+        end
+
+        def api_url
+          'https://api.github.com'
+        end
+
+        def github_hostname
+          'github.com'
+        end
+
+        def pages_hostname
+          'github.io'
+        end
+      end
+    end
+
     class << self
+      def environment
+        Jekyll.env || Pages.env || 'development'
+      end
+
       def client
         @client ||= Client.new
       end
@@ -104,9 +128,43 @@ module Jekyll
         values[key.to_s] = Value.new(value)
       end
     end
+
+    # Environment-Specific
+    register_value('environment', proc { environment })
+    register_value('hostname', proc { Pages.github_hostname })
+    register_value('pages_hostname', proc { Pages.pages_hostname })
+    register_value('api_url', proc { Pages.api_url })
+
+    register_value('versions', proc {
+      begin
+        require 'github-pages'
+        GitHubPages.versions
+      rescue LoadError; Hash.new end
+    })
+
+    # The Juicy Stuff
+    register_value('public_repositories', proc { |c| c. })
+    register_value('organization_members', [ User Objects ])
+    register_value('build_revision', 'cbd866ebf142088896cbe71422b949de7f864bce')
+    register_value('project_title', 'metadata-example')
+    register_value('project_tagline', 'A GitHub Pages site to showcase repository metadata')
+    register_value('owner_name', 'github')
+    register_value('owner_url', 'https,//github.com/github')
+    register_value('owner_gravatar_url', 'https,//github.com/github.png')
+    register_value('repository_url', 'https,//github.com/github/metadata-example')
+    register_value('repository_nwo', 'github/metadata-example')
+    register_value('repository_name', 'metadata-example')
+    register_value('zip_url', 'https,//github.com/github/metadata-example/zipball/gh-pages')
+    register_value('tar_url', 'https,//github.com/github/metadata-example/tarball/gh-pages')
+    register_value('clone_url', 'https,//github.com/github/metadata-example.git')
+    register_value('releases_url', 'https,//github.com/github/metadata-example/releases')
+    register_value('issues_url', 'https,//github.com/github/metadata-example/issues')
+    register_value('wiki_url', 'https,//github.com/github/metadata-example/wiki')
+    register_value('language', nil)
+    register_value('is_user_page', false)
+    register_value('is_project_page', true)
+    register_value('show_downloads', true)
+    register_value('url', 'http,//username.github.io/metadata-example', // (or the CNAME)
+    register_value('contributors', [ User Objects )
   end
 end
-
-Jekyll::GitHubMetadata.register_value('environment', proc { |c|
-  ENV.fetch('JEKYLL_ENV', ENV.fetch('PAGES_ENV', 'development'))
-})
