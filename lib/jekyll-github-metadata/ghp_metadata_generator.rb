cc9a34c5ac70d811e4a7b92053f20eadc2fb8dcd
Parker Moore <parkrmoore@gmail.com>
2015-07-02 15:03:33 -0700

    GHPMetadataGenerator: Don't overwrite site.github if it's already set.

--- lib/jekyll-github-metadata/ghp_metadata_generator.rb
+++ lib/jekyll-github-metadata/ghp_metadata_generator.rb
@@ -5,7 +5,15 @@ module Jekyll
     class GHPMetadataGenerator < Jekyll::Generator
       def generate(site)
         GitHubMetadata.repository = GitHubMetadata::Repository.new(site.config.fetch('repository'))
-        site.config['github'] = GitHubMetadata.to_liquid
+        site.config['github'] =
+          case site.config['github']
+          when nil
+            GitHubMetadata.to_liquid
+          when Hash
+            Jekyll::Utils.deep_merge_hashes(GitHubMetadata.to_liquid, site.config['github'])
+          else
+            site.config['github']
+          end
       end
     end
   end
