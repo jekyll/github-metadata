896197bcc38dac83093fcf012cc10367a2e61fa9
Parker Moore <parkrmoore@gmail.com>
2015-09-07 21:55:07 -0700

    specs: write test for auto_paginate, aka #18

--- spec/github_metadata_spec.rb
+++ spec/github_metadata_spec.rb
@@ -29,4 +29,10 @@ RSpec.describe(Jekyll::GitHubMetadata) do
   it 'has a global GitHub API client' do
     expect(described_class.client).to be_a(Jekyll::GitHubMetadata::Client)
   end
+
+  it 'does auto_paginate' do
+    expect(
+      described_class.client.instance_variable_get(:"@client").auto_paginate
+    ).to be(true)
+  end
 end
