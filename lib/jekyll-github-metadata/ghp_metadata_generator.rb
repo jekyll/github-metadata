e1221340b2cee52db32e1a33faaff249f524608e
Parker Moore <parkrmoore@gmail.com>
2016-02-05 10:39:46 -0800

    Ensure GitHubMetadata is init'd at the beginning of the generator

--- lib/jekyll-github-metadata/ghp_metadata_generator.rb
+++ lib/jekyll-github-metadata/ghp_metadata_generator.rb
@@ -4,7 +4,9 @@ module Jekyll
   module GitHubMetadata
     class GHPMetadataGenerator < Jekyll::Generator
       def generate(site)
+        Jekyll.logger.debug "Generator:", "Calling GHPMetadataGenerator"
         GitHubMetadata.repository = GitHubMetadata::Repository.new(site.config.fetch('repository'))
+        GitHubMetadata.init!
         site.config['github'] =
           case site.config['github']
           when nil
