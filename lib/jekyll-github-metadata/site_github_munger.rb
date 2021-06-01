5185893202b4c753fe88479bc41df0cfe9178f63
Daniel Granerud <42067409+NullableInt@users.noreply.github.com>
2021-06-01 10:46:55 +0200

    Update lib/jekyll-github-metadata/site_github_munger.rb
    Co-authored-by: Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>

--- lib/jekyll-github-metadata/site_github_munger.rb
+++ lib/jekyll-github-metadata/site_github_munger.rb
@@ -42,7 +42,8 @@ module Jekyll
 
       # Set `site.url` and `site.baseurl` if unset.
       def add_url_and_baseurl_fallbacks!
-        Jekyll::GitHubMetadata.log :debug, "Adding url and baseurl fallbacks..."
+        Jekyll::GitHubMetadata.log :debug, "Adding `url` and `baseurl` fallbacks..."
+        
         site.config["url"] ||= Value.new("url", proc { |_c, r| r.url_without_path })          
         Jekyll::GitHubMetadata.log :debug, "`url` is set to #{site.config["url"]}"
         return unless should_set_baseurl?
